8c6c72d90862cd254b79f8e04601974acde3d82c
Rafinha <arruda.rafa.ra@gmail.com>
2021-06-08 17:38:56 -0400

    Create leituraDado.asm

---
+++ leituraDado.asm
@@ -0,0 +1,10 @@
+#Exercício 1 - Leia um inteiro do teclado e escreva-o na console. Termine o programa com a chamada do sistema
+
+li a7, 5 #chamada do sistema para ler um inteiro
+ecall
+
+li a7, 1 #printa um inteiro
+ecall
+
+li a7, 10 #encerra o programa
+ecall
